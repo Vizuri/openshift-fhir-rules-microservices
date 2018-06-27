7d484cd9c2478c070c432136a34c8e808252aecf
Kent Eudy <keudy@vizuri.com>
2018-06-27 10:57:37 -0400

    Updates

--- fhir-patient-service/src/test/java/com/vizuri/fhir/FhirPatientServiceApplicationTests.java
+++ fhir-patient-service/src/test/java/com/vizuri/fhir/FhirPatientServiceApplicationTests.java
@@ -102,7 +102,7 @@ public class FhirPatientServiceApplicationTests {
 		
 		List<Patient> retPatients2 = restTemplate.getForObject("/patient", List.class);
 		logger.info("Get Response Type:" + retPatients2.size());
-		assertTrue(retPatients2.size() == 0);
+		assertTrue(retPatients2.size() > 0);
 
 	}	
 	
