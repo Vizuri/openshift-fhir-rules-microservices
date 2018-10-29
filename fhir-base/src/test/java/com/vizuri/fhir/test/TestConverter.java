f1b84677f05e7201e5da4e06e3a10280e9bcca2e
Kent Eudy <keudy@vizuri.com>
2018-10-29 15:24:56 -0400

    update

---
+++ fhir-base/src/test/java/com/vizuri/fhir/test/TestConverter.java
@@ -0,0 +1,15 @@
+package com.vizuri.fhir.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestConverter {
+
+	@Test
+	void test() {
+		//fail("Not yet implemented");
+		
+	}
+
+}
