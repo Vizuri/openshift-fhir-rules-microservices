5a46fedb4a3ae8bd00f4d90ff7d7ad471d430d0c
Kent Eudy <keudy@vizuri.com>
2018-10-31 18:00:44 -0400

    update

--- setup-project.sh
+++ setup-project.sh
@@ -5,7 +5,7 @@ oc create secret generic quay-registry \
     --type=kubernetes.io/dockerconfigjson
 
 oc secrets link default quay-registry --for=pull
-oc new-app --template=mongodb-persistent --param=DATABASE_SERVICE_NAME=fhirdb --param=MONGODB_USER=fhir --param=MONGODB_DATABASE=fhir
+oc new-app --template=mongodb-persistent --param=DATABASE_SERVICE_NAME=fhirdb --param=MONGODB_USER=fhir --param=MONGODB_PASSWORD=fhir --param=MONGODB_DATABASE=fhir
 
 oc new-project fhir-test
 
@@ -14,7 +14,7 @@ oc create secret generic quay-registry \
     --type=kubernetes.io/dockerconfigjson
 
 oc secrets link default quay-registry --for=pull
-oc new-app --template=mongodb-persistent --param=DATABASE_SERVICE_NAME=fhirdb --param=MONGODB_USER=fhir --param=MONGODB_DATABASE=fhir
+oc new-app --template=mongodb-persistent --param=DATABASE_SERVICE_NAME=fhirdb --param=MONGODB_USER=fhir --param=MONGODB_PASSWORD=fhir --param=MONGODB_DATABASE=fhir
 
 oc new-project fhir-prod
 
@@ -23,4 +23,4 @@ oc create secret generic quay-registry \
     --type=kubernetes.io/dockerconfigjson
 
 oc secrets link default quay-registry --for=pull
-oc new-app --template=mongodb-persistent --param=DATABASE_SERVICE_NAME=fhirdb --param=MONGODB_USER=fhir --param=MONGODB_DATABASE=fhir
+oc new-app --template=mongodb-persistent --param=DATABASE_SERVICE_NAME=fhirdb --param=MONGODB_USER=fhir --param=MONGODB_PASSWORD=fhir --param=MONGODB_DATABASE=fhir
