279a1e63ba2d0f20eb96f646dd87d97ac9b7a48e
Kent Eudy <keudy@vizuri.com>
2018-11-01 11:44:02 -0400

    update

--- setup-project.sh
+++ setup-project.sh
@@ -24,3 +24,4 @@ oc create secret generic quay-registry \
 
 oc secrets link default quay-registry --for=pull
 oc new-app --template=mongodb-persistent --param=DATABASE_SERVICE_NAME=fhirdb --param=MONGODB_USER=fhir --param=MONGODB_PASSWORD=fhir --param=MONGODB_DATABASE=fhir
+
